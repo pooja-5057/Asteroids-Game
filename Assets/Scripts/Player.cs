4d2e3f226b24779339ce5c6e9f21387378e2c916
pooja-5057 <pooja.borse.5057@gmail.com>
2025-07-04 14:39:52 +0530

    First Asteroids-Game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private Bullet bulletPrefab;
+    public float fireRate = 0.1f; 
+    private float fireTimer;
+    public float thrustSpeed = 1f;
+    public bool thrusting { get; private set; }
+
+    public float rotationSpeed = 0.1f;
+    private float turnDirection;
+
+    public float respawnDelay = 3f;
+    public float respawnInvulnerability = 3f;
+
+    public bool screenWrapping = true;
+    private Bounds screenBounds;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        GameObject[] boundaries = GameObject.FindGameObjectsWithTag("Boundary");
+
+
+        for (int i = 0; i < boundaries.Length; i++)
+        {
+            boundaries[i].SetActive(!screenWrapping);
+        }
+
+
+        screenBounds = new Bounds();
+        screenBounds.Encapsulate(Camera.main.ScreenToWorldPoint(Vector3.zero));
+        screenBounds.Encapsulate(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f)));
+    }
+
+    private void OnEnable()
+    {
+
+        TurnOffCollisions();
+        Invoke(nameof(TurnOnCollisions), respawnInvulnerability);
+    }
+
+    private void Update()
+    {
+        thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            turnDirection = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            turnDirection = -1f;
+        }
+        else
+        {
+            turnDirection = 0f;
+        }
+
+        fireTimer -= Time.deltaTime;
+
+        if ((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && fireTimer <= 0f)
+        {
+            Shoot();
+            fireTimer = fireRate;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrusting)
+        {
+            rb.AddForce(transform.up * thrustSpeed);
+        }
+
+        if (turnDirection != 0f)
+        {
+            rb.AddTorque(rotationSpeed * turnDirection);
+        }
+
+        if (screenWrapping)
+        {
+            ScreenWrap();
+        }
+    }
+
+    private void ScreenWrap()
+    {
+      
+        if (rb.position.x > screenBounds.max.x + 0.5f)
+        {
+            rb.position = new Vector2(screenBounds.min.x - 0.5f, rb.position.y);
+        }
+        else if (rb.position.x < screenBounds.min.x - 0.5f)
+        {
+            rb.position = new Vector2(screenBounds.max.x + 0.5f, rb.position.y);
+        }
+        else if (rb.position.y > screenBounds.max.y + 0.5f)
+        {
+            rb.position = new Vector2(rb.position.x, screenBounds.min.y - 0.5f);
+        }
+        else if (rb.position.y < screenBounds.min.y - 0.5f)
+        {
+            rb.position = new Vector2(rb.position.x, screenBounds.max.y + 0.5f);
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bullet.Project(transform.up);
+    }
+
+    private void TurnOffCollisions()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Ignore Collisions");
+    }
+
+    private void TurnOnCollisions()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = 0f;
+
+            GameManager.Instance.OnPlayerDeath(this);
+        }
+    }
+
+}
